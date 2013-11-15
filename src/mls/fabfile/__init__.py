e22bfdbea60daa7001f3d62af08d13827c477066
Thomas Massmann <thomas.massmann@it-spir.it>
2013-11-15 08:43:56 +0100

    Import modules.

--- src/mls/fabfile/__init__.py
+++ src/mls/fabfile/__init__.py
@@ -1,1 +1,12 @@
-# This package may contain traces of nuts
+# -*- coding: utf-8 -*-
+
+import bootstrap
+import client
+import database
+import frontend
+
+
+bootstrap = bootstrap  # PyFlakes
+client = client  # PyFlakes
+database = database  # PyFlakes
+frontend = frontend  # PyFlakes
