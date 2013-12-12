8ca44b473810437473d28b171027a2c3ebc26e11
Thomas Massmann <thomas@propertyshelf.com>
2013-12-12 12:03:38 -0500

    Migrated most utils command to propertyshelf.fabfile.common.

--- src/mls/fabfile/utils.py
+++ src/mls/fabfile/utils.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from chef import autoconfigure, Search
-from datetime import datetime
 from fabric import api
 from propertyshelf.fabfile.common.exceptions import err
 
@@ -18,44 +17,3 @@ def mls_config():
             return node.object['mls']
         except KeyError:
             return {}
-
-
-def backup_dev_packages(folder=None, user=None):
-    """Backup the development packages."""
-    config = mls_config()
-    folder = folder or config.get('app', {}).get('dir') or err('Folder must be set!')
-    user = user or config.get('user') or err('MLS user must be set!')
-    with api.settings(sudo_user=user):
-        # Backup buildout src packages.
-        with api.cd(folder):
-            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            backup_folder = 'src_backups/%s' % now
-            api.sudo('mkdir -p %s' % backup_folder)
-            api.sudo('mv ./src/* %s' % backup_folder)
-
-
-def update_dev_packes(folder=None, user=None):
-    """Update the development packages."""
-    config = mls_config()
-    folder = folder or config.get('app', {}).get('dir') or err('Folder must be set!')
-    user = user or config.get('user') or err('MLS user must be set!')
-    with api.settings(sudo_user=user):
-        # Update buildout src packages.
-        with api.cd(folder):
-            api.sudo('./bin/develop up -f')
-
-
-def run_buildout(folder=None, user=None):
-    """Run the buildout."""
-    config = mls_config()
-    folder = folder or config.get('app', {}).get('dir') or err('Folder must be set!')
-    user = user or config.get('user') or err('MLS user must be set!')
-    with api.settings(sudo_user=user):
-        # Run buildout.
-        with api.cd(folder):
-            api.sudo('./bin/buildout')
-
-
-def supervisorctl(command=None, service=None):
-    """Control supervisor services."""
-    api.sudo('supervisorctl %s %s' % (command, service), warn_only=True)
